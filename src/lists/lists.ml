02cd172470c121d8011cc5ee173028c44af87636
Sergey Shpak <serge1272@mail.ru>
2016-12-11 23:11:52 +0100

    Add functions to Lists module

--- src/lists/lists.ml
+++ src/lists/lists.ml
@@ -1,7 +1,45 @@
+let rec print_list (l: string list) = match l with
+    [] -> print_endline
+  | [el] -> print_string el ; print_endline
+  | h :: t -> print_string h ; print_string " "; print_list t
+;;
+
 let rec last (l: 'a list) = match l with
     [] -> None
   | [el] -> Some el
   | h :: t -> last t
 ;;
 
-(* if !Sys.interactive then () else main ();; *)
+let rec last_two (l: 'a list) = match l with
+    [] -> None
+  | [el] -> None
+  | first :: second :: [] -> Some (first, second)
+  | head :: tail -> last_two tail
+;;
+
+let rec at (i: int) (l: 'a list) =
+  if i < 1 then None
+  else 
+    match l with 
+      [] -> None
+    | h :: t -> match i with
+        1 -> Some h
+      | _ -> at (i - 1) t 
+;;
+
+let length (l: 'a list) =
+    let rec aux (l: 'a list) (acc: int) = match l with
+        [] -> acc
+      | [el] -> acc + 1
+      | h :: t -> aux t (acc + 1) 
+    in
+    aux l 0
+;;
+
+let rev (l: 'a list) =
+  let rec aux (l: 'a list) (acc: 'a list) = match l with
+      [] -> acc
+    | h :: t -> aux t ([h] @ acc)
+  in
+  aux l []
+;;
--- src/lists/tests.ml
+++ src/lists/tests.ml
@@ -1,18 +1,78 @@
 open OUnit2;;
 
 let test_ListsLast_ReturnsLastElement ctx = 
-  assert_equal (Some "d") (Lists.last[ "a" ; "b" ; "c" ; "d"])
+  assert_equal (Some "d") (Lists.last [ "a" ; "b" ; "c" ; "d"])
 ;;
 
 let test_ListsLast_ReturnsNoneIfListIsEmpty ctx =
-  assert_equal (None) (Lists.last[])
+  assert_equal None (Lists.last[])
 ;;
+
+let test_ListsLastTwo_ReturnsLastTwoElements ctx =
+  assert_equal (Some ("c", "d")) (Lists.last_two [ "a" ; "b" ; "c" ; "d"]);
+;;
+
+let test_ListsLastTwo_ReturnsNoneIfListContainsLessThanTwoElements ctx =
+  assert_equal None (Lists.last_two ["a"]);
+  assert_equal None (Lists.last_two []);
+;;
+
+let test_ListsAt_ReturnsElementAtGivenPosition ctx =
+  assert_equal (Some "c") (Lists.at 3 [ "a" ; "b"; "c"; "d"; "e" ])
+;;
+
+let test_ListsAt_ReturnsNoneIfIndexIsOutOfBound ctx =
+  assert_equal None (Lists.at 3 ["a"]);
+  assert_equal None (Lists.at 0 ["a"]);
+  assert_equal None (Lists.at (-1) ["a"]);
+;;
+
+let test_ListsAt_ReturnsNoneIfListIsEmpty ctx =
+  assert_equal None (Lists.at 1 []);
+;;
+
+let test_ListsLength_ReturnsNumberOfElementsInList ctx =
+  assert_equal 3 (Lists.length [ "a" ; "b" ; "c"]);
+  assert_equal 0 (Lists.length []);
+;;
+
+let test_ListsRev_ReturnsReversedList ctx =
+  assert_equal [ "a" ; "b" ; "c" ] (Lists.rev [ "c" ; "b" ; "a" ])
+;;
+
+let test_ListsRev_SingleElementListStaysSame ctx =
+  assert_equal [ "a" ] (Lists.rev [ "a" ])
+;;
+
+let test_ListsRev_EmptyListStaysSame ctx =
+  assert_equal [] (Lists.rev [])
+;;
+
 
 let suite = 
   "suite">:::
   ["Lists.last: Returns last element">:: test_ListsLast_ReturnsLastElement ;
    "Lists.last: Returns 'None' if list is empty">:: 
-   test_ListsLast_ReturnsNoneIfListIsEmpty]
+        test_ListsLast_ReturnsNoneIfListIsEmpty ;
+   "Lists.last_two: Returns last two elements">:: 
+        test_ListsLastTwo_ReturnsLastTwoElements ;
+   "Lists.last_two: Returns None if list contains less than two elements">::
+        test_ListsLastTwo_ReturnsNoneIfListContainsLessThanTwoElements ;
+   "Lists.at: Return element at given position">::
+        test_ListsAt_ReturnsElementAtGivenPosition ;
+   "Lists.at: Returns None if given index is out of bound">::
+        test_ListsAt_ReturnsNoneIfIndexIsOutOfBound ;
+   "Lists.at: Returns None if list is empty">:: 
+        test_ListsAt_ReturnsNoneIfListIsEmpty ;
+   "Lists.length: Return number of elements in list">::
+        test_ListsLength_ReturnsNumberOfElementsInList ;
+   "Lists.rev: Returns reversed list">::
+        test_ListsRev_ReturnsReversedList ;
+   "Lists.rev: List that consist of a single element stays the same">::
+        test_ListsRev_SingleElementListStaysSame ;
+   "Lists.rev: Empty list stays the same">::
+        test_ListsRev_EmptyListStaysSame ;
+  ]
 ;;
 
 let () =
